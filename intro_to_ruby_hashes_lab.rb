df6138b5c3343c0ca4b09942a8c5a6f29ec29432
Brian Nicholls <nichol88@gmail.com>
2019-11-01 06:13:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,9 @@ def monopoly_with_third_tier
 		},
 		:names => {
 			:reading_railroad => {},
-			:reading_railroad => {},
-			:reading_railroad => {},
-			:reading_railroad => {}
+			:short_line_railroad => {},
+			:pennsylvania_railroad => {},
+			:b_and_o_railroad => {}
 		}
 		}}
 end
